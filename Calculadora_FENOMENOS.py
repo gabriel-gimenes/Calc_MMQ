fb2ef7d89dbecdf4427fe14c79d2e3c4a689d44a
Gabriel Gimenes Cardoso Silva <59775330+gabriel-gimenes@users.noreply.github.com>
2020-08-31 23:47:58 -0300

    Update Calculadora_FENOMENOS.py

--- Calculadora_FENOMENOS.py
+++ Calculadora_FENOMENOS.py
@@ -8,26 +8,28 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+'''    
+Exemplo de Teste:
+x = np.array([-2.09,-1.76,-1.51,-1.22,-.92,-.69,-.56,-.41,-.25,-.07])
+y = np.array([0.002915,.002976,.00303,.003086,.003154,.003215,.003247,
+                .003279,.003322,.003356])
+alpha_square = np.array([4E-06,4E-06,4E-06,5E-06,5E-06,5E-06,5E-06,
+                            5E-06,6E-06,6E-06])**2
+'''
 def main():
     lista_exp_data = []
     lista_summation = []
     ix_exp_data = []
     print("Calculadora de MMQ")
-    '''
+    #Inserção de dados do experimento
     n = int(input('Qual o número de dados a ser computado?: '))
     x = np.array([float(input(f'Digite o valor de x[{x}]: ')) for x in range(n)])
     y = np.array([float(input(f'Digite o valor de y[{y}]: ')) for y in range(n)])
     alpha_square = np.array([(float(input(f'Digite o erro experimental de y[{y}]: ')))**2 for y in range(n)])
-    '''
     
-    x = np.array([-2.09,-1.76,-1.51,-1.22,-.92,-.69,-.56,-.41,-.25,-.07])
-    y = np.array([0.002915,.002976,.00303,.003086,.003154,.003215,.003247,
-                  .003279,.003322,.003356])
-    alpha_square = np.array([4E-06,4E-06,4E-06,5E-06,5E-06,5E-06,5E-06,
-                             5E-06,6E-06,6E-06])**2
 
 #Criando DataFrame para os dados digitados pelo usuário
-    for i in range(10):
+    for i in range(n):
         lista_exp_data.append([x[i],y[i],alpha_square[i]])
         ix_exp_data.append(i)    
         
@@ -46,7 +48,7 @@ def main():
              one_over_alpha_square.sum(),xi_mult_yi_over_alpha_square.sum(),
              xi_square_over_alpha_square.sum()]
     
-    for i in range(10):
+    for i in range(n):
         lista_summation.append([yi_over_alpha_square[i],xi_over_alpha_square[i],one_over_alpha_square[i],xi_mult_yi_over_alpha_square[i],xi_square_over_alpha_square[i]])
         
     ix_exp_data.append('Total')
@@ -56,20 +58,41 @@ def main():
     print(30*'-' + 'Summation' + 30*'-')
     print(summation)
     
-#Calculando coeficientes a e b, além de suas respectivas incertezas
+#Calculando coeficientes a e b, além de suas respectivas incertezas (pendente)
     
     a = ((total[0]*total[1])-(total[2]*total[3]))/((total[1]**2) - (total[4]*total[2]))
     
     b = (total[0]-a*total[1])/total[2]
     
     print(f'\nSlope: {a}, Linear Coefficient: {b}')
-
+#plot do gráfico com os pontos das coordenadas x e y, além da reta que melhor se ajusta.
     y1 = a*x+b
     plt.title('Graph of  x and y')
     plt.xlabel('x-values')
     plt.ylabel('y-values')
     #plt.yticks([0.0025,0.0030,0.0035,0.0040,0.0045,0.0050])
-    plt.ylim(0,.004)
+    if x.min() < 0:
+        xmin = x.min()*1.1
+    else:
+        xmin = x.min()*0.9
+    
+    if x.max() < 0:
+        xmax = x.max()*0.9
+    else:
+        xmax = x.max()*1.1
+
+    if y.min() < 0:
+        ymin = y.min()*1.1
+    else:
+        ymin = y.min()*0.9
+    
+    if y.max() < 0:
+        ymax = y.max()*0.9
+    else:
+        ymax = y.max()*1.1
+
+    plt.xlim(xmin,xmax)
+    plt.ylim(ymin,ymax)
     plt.scatter(x,y)
     plt.plot(x,y1)
     plt.show()
